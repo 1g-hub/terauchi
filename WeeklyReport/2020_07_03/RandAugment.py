a9c844ca4dc6ee10d5ee16c21f2c3707dcb21a2b
tonnnura <terauchi@ss.cs.osakafu-u.ac.jp>
2020-07-03 05:47:35 +0900

    add report

---
+++ WeeklyReport/2020_07_03/RandAugment.py
@@ -0,0 +1,14 @@
+transforms = [
+"Identity", "AutoContrast", "Equalize",
+"Rotate", "Solarize", "Color", "Posterize",
+"Contrast", "Brightness", "Sharpness",
+"ShearX", "ShearY", "TranslateX", "TranslateY"]
+def randaugment(N, M):
+    """Generate a set of distortions.
+    Args:
+    N: Number of augmentation transformations to
+    apply sequentially.
+    M: Magnitude for all the transformations.
+    """
+    sampled_ops = np.random.choice(transforms, N)
+    return [(op, M) for op in sampled_ops]
